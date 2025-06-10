9ccc09d3d321bd957d5ab815445cb1e7d84bb265
Jeffrey Mark Siskind <qobi@purdue.edu>
2025-06-10 10:46:35 -0400

    $-$

--- src/tables.py
+++ src/tables.py
@@ -1,4 +1,3 @@
-#\needswork: to replace - with $-$ in numbers
 import pandas
 
 def denan(x):
@@ -6,6 +5,10 @@ def denan(x):
         return x
     else:
         return ""
+
+def latexfloat(x):
+    return ("%.2f"%x).replace("-", "$-$", 1)
+
 
 def table2():
     f = open("figures/table2.tex", "w")
@@ -16,25 +19,25 @@ def table2():
     f.write("\\textbf{Primary models} & & & \\\\\n")
     for model in primary_models:
         idx = list(cdbsu["model"]).index(model)
-        f.write("\\quad %s & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f \\\\\n"%(
+        f.write("\\quad %s & %s $\pm$ %s & %s $\pm$ %s & %s $\pm$ %s \\\\\n"%(
             cdbsu["model"][idx],
-            float(cdbsu["confounded_test"][idx]),
-            float(cdbsu["confounded_test_std"][idx]),
-            float(cdbsu["unconfounded_test"][idx]),
-            float(cdbsu["unconfounded_test_std"][idx]),
-            float(cdbsu["bias"][idx]),
-            float(cdbsu["bias_std"][idx])))
+            latexfloat(cdbsu["confounded_test"][idx]),
+            latexfloat(cdbsu["confounded_test_std"][idx]),
+            latexfloat(cdbsu["unconfounded_test"][idx]),
+            latexfloat(cdbsu["unconfounded_test_std"][idx]),
+            latexfloat(cdbsu["bias"][idx]),
+            latexfloat(cdbsu["bias_std"][idx])))
     f.write("\\textbf{Additional models} & & & \\\\\n")
     for model in additional_models:
         idx = list(cdbsu["model"]).index(model)
-        f.write("\\quad %s & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f \\\\\n"%(
+        f.write("\\quad %s & %s $\pm$ %s & %s $\pm$ %s & %s $\pm$ %s \\\\\n"%(
             cdbsu["model"][idx],
-            float(cdbsu["confounded_test"][idx]),
-            float(cdbsu["confounded_test_std"][idx]),
-            float(cdbsu["unconfounded_test"][idx]),
-            float(cdbsu["unconfounded_test_std"][idx]),
-            float(cdbsu["bias"][idx]),
-            float(cdbsu["bias_std"][idx])))
+            latexfloat(cdbsu["confounded_test"][idx]),
+            latexfloat(cdbsu["confounded_test_std"][idx]),
+            latexfloat(cdbsu["unconfounded_test"][idx]),
+            latexfloat(cdbsu["unconfounded_test_std"][idx]),
+            latexfloat(cdbsu["bias"][idx]),
+            latexfloat(cdbsu["bias_std"][idx])))
     f.write("\\bottomrule\n")
     f.write("\\end{tabular}\n")
     f.close()
@@ -48,25 +51,25 @@ def table3():
     f.write("\\textbf{Primary models} & & & \\\\\n")
     for model in primary_models:
         idx = list(pdbs["model"]).index(model)
-        f.write("\\quad %s & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f \\\\\n"%(
+        f.write("\\quad %s & %s $\pm$ %s & %s $\pm$ %s & %s $\pm$ %s \\\\\n"%(
             pdbs["model"][idx],
-            float(pdbs["confounded_test"][idx]),
-            float(pdbs["confounded_test_std"][idx]),
-            float(pdbs["unconfounded_test"][idx]),
-            float(pdbs["unconfounded_test_std"][idx]),
-            float(pdbs["bias"][idx]),
-            float(pdbs["bias_std"][idx])))
+            latexfloat(pdbs["confounded_test"][idx]),
+            latexfloat(pdbs["confounded_test_std"][idx]),
+            latexfloat(pdbs["unconfounded_test"][idx]),
+            latexfloat(pdbs["unconfounded_test_std"][idx]),
+            latexfloat(pdbs["bias"][idx]),
+            latexfloat(pdbs["bias_std"][idx])))
     f.write("\\textbf{Additional models} & & & \\\\\n")
     for model in additional_models:
         idx = list(pdbs["model"]).index(model)
-        f.write("\\quad %s & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f & %.2f $\pm$ %.2f \\\\\n"%(
+        f.write("\\quad %s & %s $\pm$ %s & %s $\pm$ %s & %s $\pm$ %s \\\\\n"%(
             pdbs["model"][idx],
-            float(pdbs["confounded_test"][idx]),
-            float(pdbs["confounded_test_std"][idx]),
-            float(pdbs["unconfounded_test"][idx]),
-            float(pdbs["unconfounded_test_std"][idx]),
-            float(pdbs["bias"][idx]),
-            float(pdbs["bias_std"][idx])))
+            latexfloat(pdbs["confounded_test"][idx]),
+            latexfloat(pdbs["confounded_test_std"][idx]),
+            latexfloat(pdbs["unconfounded_test"][idx]),
+            latexfloat(pdbs["unconfounded_test_std"][idx]),
+            latexfloat(pdbs["bias"][idx]),
+            latexfloat(pdbs["bias_std"][idx])))
     f.write("\\bottomrule\n")
     f.write("\\end{tabular}\n")
     f.close()
@@ -80,21 +83,21 @@ def table4():
     f.write("\\multicolumn{2}{l}{\\textbf{Primary models}}\\\\\n")
     for model in primary_models:
         idx = list(cdbsi["model"]).index(model)
-        f.write("\\quad %s & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f]\\\\\n"%(
+        f.write("\\quad %s & %s\\rlap{\\textsuperscript{%s}} & [%s, %s]\\\\\n"%(
             cdbsi["model"][idx],
-            float(cdbsi["estimate"][idx]),
+            latexfloat(cdbsi["estimate"][idx]),
             cdbsi["significance"][idx],
-            float(cdbsi["lower_bound"][idx]),
-            float(cdbsi["upper_bound"][idx])))
+            latexfloat(cdbsi["lower_bound"][idx]),
+            latexfloat(cdbsi["upper_bound"][idx])))
     f.write("\\multicolumn{2}{l}{\\textbf{Additional models}}\\\\\n")
     for model in additional_models:
         idx = list(cdbsi["model"]).index(model)
-        f.write("\\quad %s & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f]\\\\\n"%(
+        f.write("\\quad %s & %s\\rlap{\\textsuperscript{%s}} & [%s, %s]\\\\\n"%(
             cdbsi["model"][idx],
-            float(cdbsi["estimate"][idx]),
+            latexfloat(cdbsi["estimate"][idx]),
             cdbsi["significance"][idx],
-            float(cdbsi["lower_bound"][idx]),
-            float(cdbsi["upper_bound"][idx])))
+            latexfloat(cdbsi["lower_bound"][idx]),
+            latexfloat(cdbsi["upper_bound"][idx])))
     f.write("\\bottomrule\n")
     f.write("\\multicolumn{3}{p{0.7\\linewidth}}{\\small `***' indicates that the estimate is different from 1 at the $p < 0.001$ significance level.}\n")
     f.write("\\end{tabular}\n")
@@ -111,30 +114,30 @@ def table5():
     f.write("\\midrule\n")
     f.write("\\textbf{Fixed effects} & & \\\\\n")
     #\needswork: I'm not sure the next two are correct.
-    f.write("\\quad $\\beta_{0}$ & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] \\\\\n"%(
-        float(fe["Estimate"][0]),
+    f.write("\\quad $\\beta_{0}$ & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] \\\\\n"%(
+        latexfloat(fe["Estimate"][0]),
         fe["Sig"][0],
-        float(fe["2.5_ci"][0]),
-        float(fe["97.5_ci"][0])))
-    f.write("\\quad $\\beta_{1}$ & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] \\\\\n"%(
-        float(fe["Estimate"][1]),
+        latexfloat(fe["2.5_ci"][0]),
+        latexfloat(fe["97.5_ci"][0])))
+    f.write("\\quad $\\beta_{1}$ & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] \\\\\n"%(
+        latexfloat(fe["Estimate"][1]),
         fe["Sig"][1],
-        float(fe["2.5_ci"][1]),
-        float(fe["97.5_ci"][1])))
+        latexfloat(fe["2.5_ci"][1]),
+        latexfloat(fe["97.5_ci"][1])))
     f.write("\\textbf{Random effects} & & \\\\\n")
     #\needswork: I'm not sure the next two are correct.
-    f.write("\\quad $\\sigma^2_{m}$ & %.2f & [%.2f,  %.2f] \\\\\n"%(
-        float(re["Var"][0]),
-        float(ci["2.5 %"][0]),
-        float(ci["97.5 %"][0])))
-    f.write("\\quad $\\sigma^2_{s}$ & %.2f & [%.2f,  %.2f] \\\\\n"%(
-        float(re["Var"][1]),
-        float(ci["2.5 %"][1]),
-        float(ci["97.5 %"][1])))
-    f.write("\\quad $\\sigma^2$ & %.2f & [%.2f,  %.2f] \\\\\n"%(
-        float(re["Var"][2]),
-        float(ci["2.5 %"][2]),
-        float(ci["97.5 %"][2])))
+    f.write("\\quad $\\sigma^2_{m}$ & %s & [%s,  %s] \\\\\n"%(
+        latexfloat(re["Var"][0]),
+        latexfloat(ci["2.5 %"][0]),
+        latexfloat(ci["97.5 %"][0])))
+    f.write("\\quad $\\sigma^2_{s}$ & %s & [%s,  %s] \\\\\n"%(
+        latexfloat(re["Var"][1]),
+        latexfloat(ci["2.5 %"][1]),
+        latexfloat(ci["97.5 %"][1])))
+    f.write("\\quad $\\sigma^2$ & %s & [%s,  %s] \\\\\n"%(
+        latexfloat(re["Var"][2]),
+        latexfloat(ci["2.5 %"][2]),
+        latexfloat(ci["97.5 %"][2])))
     f.write("\\bottomrule\n")
     f.write("\\multicolumn{3}{p{0.7\\linewidth}}{\\small `***' indicates that the estimate is different from 1 at the $p < 0.001$ significance level.}\n")
     f.write("\\end{tabular}\n")
@@ -154,11 +157,11 @@ def table6():
                       "Human Face",
                       "Natural Object"]:
         idx = list(cbe["category"]).index(parameter)
-        f.write("\\quad %s & %.2f & [%.2f, %.2f] \\\\\n"%(
+        f.write("\\quad %s & %s & [%s, %s] \\\\\n"%(
             cbe["category"][idx],
-            float(cbe["Estimate"][idx]),
-            float(cbe["2.5_ci"][idx]),
-            float(cbe["97.5_ci"][idx])))
+            latexfloat(cbe["Estimate"][idx]),
+            latexfloat(cbe["2.5_ci"][idx]),
+            latexfloat(cbe["97.5_ci"][idx])))
     f.write("\\textbf{Contrast} & & \\\\\n")
     for jdx, contrast in enumerate(["Animal Body - Animal Face",
                                     "Animal Body - Artificial Object",
@@ -176,18 +179,18 @@ def table6():
                                     "Human Body - Natural Object",
                                     "Human Face - Natural Object"]):
         idx = list(cbc["Contrast"]).index(contrast)
-        f.write("\\quad %s & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] \\\\\n"%(
+        f.write("\\quad %s & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] \\\\\n"%(
             canonical_contrasts[jdx],
-            float(cbc["Estimate"][idx]),
+            latexfloat(cbc["Estimate"][idx]),
             denan(cbc["Sig"][idx]),
-            float(cbc["2.5_ci"][idx]),
-            float(cbc["97.5_ci"][idx])))
+            latexfloat(cbc["2.5_ci"][idx]),
+            latexfloat(cbc["97.5_ci"][idx])))
     f.write("\\textbf{Random effects} & &\\\\\n")
     #\needswork: I'm not sure the next three are correct.
     #\needswork: should index by name instead of [0], [1], and [2]
-    f.write("\\quad $\\sigma_{s}^2$ & %.2f & \\\\\n"%float(re["Var"][0]))
-    f.write("\\quad $\\sigma_{m}^2$ & %.2f & \\\\\n"%float(re["Var"][1]))
-    f.write("\\quad $\\sigma^2$ & %.2f & \\\\\n"%float(re["Var"][2]))
+    f.write("\\quad $\\sigma_{s}^2$ & %s & \\\\\n"%latexfloat(re["Var"][0]))
+    f.write("\\quad $\\sigma_{m}^2$ & %s & \\\\\n"%latexfloat(re["Var"][1]))
+    f.write("\\quad $\\sigma^2$ & %s & \\\\\n"%latexfloat(re["Var"][2]))
     f.write("\\bottomrule\n")
     f.write("\\multicolumn{3}{p{0.85\\linewidth}}{\\small `*', `**', and `***' indicate that the estimate is different from zero at the $p < 0.05$, $p < 0.01$, and $p < 0.001$ significance levels, respectively.}\n")
     f.write("\\end{tabular}\n")
@@ -204,29 +207,29 @@ def table7():
     f.write("\\textbf{Affected models}\\\\\n")
     for model in primary_models:
         idx = list(pdas["Unnamed: 0"]).index(model)
-        f.write("\\quad %s & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] \\\\\n"%(
+        f.write("\\quad %s & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] \\\\\n"%(
             pdas["Unnamed: 0"][idx],
-            float(pdas["estimate"][idx]),
+            latexfloat(pdas["estimate"][idx]),
             denan(pdas["significance"][idx]),
-            float(pdas["lower_bound"][idx]),
-            float(pdas["upper_bound"][idx]),
-            float(pdas["estimate.1"][idx]),
+            latexfloat(pdas["lower_bound"][idx]),
+            latexfloat(pdas["upper_bound"][idx]),
+            latexfloat(pdas["estimate.1"][idx]),
             denan(pdas["significance.1"][idx]),
-            float(pdas["lower_bound.1"][idx]),
-            float(pdas["upper_bound.1"][idx])))
+            latexfloat(pdas["lower_bound.1"][idx]),
+            latexfloat(pdas["upper_bound.1"][idx])))
     f.write("\\textbf{Additional models} & & & & \\\\\n")
     for model in additional_models:
         idx = list(pdas["Unnamed: 0"]).index(model)
-        f.write("\\quad %s & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] & %.2f\\rlap{\\textsuperscript{%s}} & [%.2f, %.2f] \\\\\n"%(
+        f.write("\\quad %s & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] & %s\\rlap{\\textsuperscript{%s}} & [%s, %s] \\\\\n"%(
             pdas["Unnamed: 0"][idx],
-            float(pdas["estimate"][idx]),
+            latexfloat(pdas["estimate"][idx]),
             denan(pdas["significance"][idx]),
-            float(pdas["lower_bound"][idx]),
-            float(pdas["upper_bound"][idx]),
-            float(pdas["estimate.1"][idx]),
+            latexfloat(pdas["lower_bound"][idx]),
+            latexfloat(pdas["upper_bound"][idx]),
+            latexfloat(pdas["estimate.1"][idx]),
             denan(pdas["significance.1"][idx]),
-            float(pdas["lower_bound.1"][idx]),
-            float(pdas["upper_bound.1"][idx])))
+            latexfloat(pdas["lower_bound.1"][idx]),
+            latexfloat(pdas["upper_bound.1"][idx])))
     f.write("\\bottomrule\n")
     f.write("\\multicolumn{5}{p{0.9\\linewidth}}{\\small `*', `**', and `***' indicate that the estimate is different from chance level (8.25\\%) at the $p < 0.05$, $p < 0.01$, and $p < 0.001$ significance levels, respectively.}\n")
     f.write("\\end{tabular}\n")
